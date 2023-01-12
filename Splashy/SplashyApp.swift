30632b1c02cb726fa391c512f9cc2e17d6cd617e
Greg Price <greg@madebyhive.com>
2023-01-12 15:49:46 +0000

    Initial Commit

---
+++ Splashy/SplashyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashyApp.swift
+//  Splashy
+//
+//  Created by Greg Price on 12/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct SplashyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
