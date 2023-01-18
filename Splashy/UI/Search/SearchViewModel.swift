11be27e296cd34d088fc14aae339da4775901a72
Greg Price <greg@madebyhive.com>
2023-01-18 15:43:32 +0000

    Updated number of search results now using 3 column grid

--- Splashy/UI/Search/SearchViewModel.swift
+++ Splashy/UI/Search/SearchViewModel.swift
@@ -10,7 +10,7 @@ extension SearchView {
         init() {
             $searchTerm
                 .debounce(for: .seconds(0.3), scheduler: DispatchQueue.global())
-                .map { unsplash.call(.searchPhotos(query: $0)).map(\SearchResponse.results) }
+                .map { unsplash.call(.searchPhotos(query: $0, perPage: 21)).map(\SearchResponse.results) }
                 .switchToLatest()
                 .replaceError(with: [Photo]())
                 .assign(to: &$searchResults)
